641b2f9d7784bcf6f8813f6efc79ad5860aac131
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2023-12-30 22:05:04 +0000

    Update information_theory.py

--- information_theory.py
+++ information_theory.py
@@ -4,11 +4,3 @@ from gzip import compress
 K = lambda text: len(compress(text.encode('utf-8')))
 
 information_distance = lambda x,y: K(x+y) - min(K(x), K(y))
-
-rank = lambda target,candidates,top_n: sorted( 
-  candidates,
-  key=lambda candidate:information_distance(
-    x=target,
-    y=candidate
-  ) 
-)[:top_n]
